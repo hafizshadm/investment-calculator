603f973864cae316b0dca8a45e8e667bf77320a4
Shad Muhammad <hafizshad.pk@gmail.com>
2024-10-18 16:23:35 +0500

    Lifting state up on userinput and results component started

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import { useState } from "react"
 import Header from './components/Header'
 import UserInput from './components/UserInput'
+import Results from "./components/Results";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 1000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+  });
+
+  function handleChange(inputIdentifies, newValue) {
+    setUserInput(prevUserInput => {
+      return {
+        ...prevUserInput,
+        [inputIdentifies]: newValue
+      }
+    })
+  }
+
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      <Results input={userInput} />
     </>
 
   )
